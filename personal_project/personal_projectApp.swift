dc036d779e06cf0b3a0ef3ebfe386900b12a9e04
Jordan Evans <77519478+jordanaevanz@users.noreply.github.com>
2022-09-19 14:11:43 -0600

    first commit of personal project

---
+++ personal_project/personal_projectApp.swift
@@ -0,0 +1,17 @@
+//
+//  personal_projectApp.swift
+//  personal_project
+//
+//  Created by Jordan Evans on 9/13/22.
+//
+
+import SwiftUI
+
+@main
+struct personal_projectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
